3b11acd36e6600bb61f8be451e220656cf60fdb8
danonymous856 <donaldlaishram861@gmail.com>
2022-07-14 20:20:16 +0530

    Pre-order traversal description added

--- Binary Tree/Traversal Algorithms/pre_order_traversal.py
+++ Binary Tree/Traversal Algorithms/pre_order_traversal.py
@@ -6,7 +6,6 @@ Check if the current node is empty/null.
 Display the data part of the root (or current node).
 Traverse the left subtree by recursively calling the pre-order method.
 Traverse the right subtree by recursively calling the pre-order method.
-
 """
 
 def preorder_print(self,start,traversal):
