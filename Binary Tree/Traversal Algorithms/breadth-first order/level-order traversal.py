34265f6b0ca91a4354ad96d650aa149b8ebd18da
danonymous856 <donaldlaishram861@gmail.com>
2022-07-27 21:32:53 +0530

    level order traversal completed.

--- Binary Tree/Traversal Algorithms/breadth-first order/level-order traversal.py
+++ Binary Tree/Traversal Algorithms/breadth-first order/level-order traversal.py
@@ -91,10 +91,12 @@ class BinaryTree(object):
         return traversal
 
 
-tree = BinaryTree(1)
-tree.root.left = Node(2)
-tree.root.right = Node(3)
-tree.root.left.left = Node(4)
+tree = BinaryTree(12)
+tree.root.left = Node(32)
+tree.root.right = Node(37)
+tree.root.left.left = Node(24)
 tree.root.left.right = Node(5)
+tree.root.right.left = Node(100)
+tree.root.right.right = Node(75)
 
-print(tree.print_tree("levelorder"))
+print(tree.print_tree("preorder"))
