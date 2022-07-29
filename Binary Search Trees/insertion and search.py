208e4c5bb4537b0aab0e17fc9a7b9405d444e448
danonymous856 <donaldlaishram861@gmail.com>
2022-07-29 23:59:24 +0530

    BST has one mandatory rule:traverse to left subtree if new node is less than the current node else traverse to right subtree

---
+++ Binary Search Trees/insertion and search.py
@@ -0,0 +1,37 @@
+class Node(object):
+    def __int__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BST(object):
+    def __int__(self,root):
+        self.root = Node(root)
+
+    def search(self,target):
+        self.serach_helper(self.root,target)
+
+    def insertion(self,new_val):
+        self.insert_helper(self.root, new_val)
+
+    def insert_helper(self, start, new_val):
+        if start.value < new_val:
+            if start.right:
+                self.insert_helper(start.right, new_val)
+            else:
+                start.right = Node(new_val)
+        else:
+            if start.left:
+                self.insert_helper(start.left, new_val)
+            else:
+                start.left = Node(new_val)
+        pass
+
+    def serach_helper(self, current, target):
+        if current:
+            if current.value == target:
+                return True
+            elif current.value < target:
+                return self.serach_helper(current.right, target)
+
+            return self.serach_helper(current.left,target)
