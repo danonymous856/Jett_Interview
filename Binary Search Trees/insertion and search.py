f2932c779b8b5e9601ed0ec546ca4b923e8d99da
danonymous856 <donaldlaishram861@gmail.com>
2022-07-31 21:44:07 +0530

    insertion and search functions are invoked in binarytree class

--- Binary Search Trees/insertion and search.py
+++ Binary Search Trees/insertion and search.py
@@ -33,4 +33,22 @@ class BST(object):
             elif current.value < target:
                 return self.serach_helper(current.right, target)
 
-            return self.serach_helper(current.left,target)
+            return self.serach_helper(current.left, target)
+
+    def is_bst_satisfied(self):
+        def helper(node, lower=float('-inf'), upper=float('inf')):
+            if not node:
+                return True
+
+            val = node.data
+            if val <= lower or val >= upper:
+                return False
+
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            return True
+
+        return helper(self.root)
+
