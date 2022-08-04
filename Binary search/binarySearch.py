f1e7bdbc40c960133fe5b81f0a7afba002bb2be5
danonymous856 <donaldlaishram861@gmail.com>
2022-08-05 00:14:41 +0530

    BInary search done with Binary tree class invoke

--- Binary search/binarySearch.py
+++ Binary search/binarySearch.py
@@ -8,13 +8,25 @@ class BinaryTree(object):
     def __int__(self,root):
         self.root = Node(root)
 
-    def BinarySearch(self,arr,start,end,mid,target):
+    def BinarySearch(self,arr,start,end,target):
         """
         Recursive Binary Search
         """
-        if target == arr[mid]:
-            return  mid
-        elif target <= arr[mid]:
-            return self.BinarySearch(arr,start,mid-1,mid,target)
+        while start <= end:
+            mid = (start+end)//2
+            if target == arr[mid]:
+                return  mid
+            elif target <= arr[mid]:
+                return self.BinarySearch(arr,start,mid-1,target)
+            elif target > arr[mid]:
+                return self.BinarySearch(arr,mid+1,end,target)
+            return -1
 
-        return self.BinarySearch(arr,mid+1,end,mid,target)
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(array)
+start = int(0)
+end = int(len(array) - 1)
+target = int(input("enter the number you want to find index for:"))
+
+tree = BinaryTree()
+print(tree.BinarySearch(array, start, end, target))
